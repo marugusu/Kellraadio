baa28d28130a97affabb17cfa06929446905c8eb
margusra <margus.ratsepp@remitk.ee>
2026-09-07 23:54:51 +0300

    chore: bump version to 1.1.9 (versionCode 11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "app.radiorecalarm"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.8"
+        versionCode = 11
+        versionName = "1.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
